b5e017571f4e2a6f06a4c4f11f0c0f5e714defa2
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2023-11-02 16:34:16 +0100

    starting over

--- drizzle/0000_perpetual_midnight.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" varchar,
-	"surname" varchar,
-	"email" varchar
-);
--- src/db/schema.ts
+++
@@ -1,9 +0,0 @@
-import { pgTable, serial, text, varchar } from "drizzle-orm/pg-core";
-
-export const schema = pgTable("users", {
-    id:serial("id").primaryKey(),
-    name:varchar("name"),
-    surname:varchar("surname"),
-    email:varchar("email"),
-    password:text("password"),
-});
