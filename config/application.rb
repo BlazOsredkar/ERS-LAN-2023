7050160c1c8abb5cd70e9e822ca0561e6302f562
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2023-11-03 00:49:34 +0100

    added landing page, added login, added bootstrap

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module ERS_LAN
+module ERSLan
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
