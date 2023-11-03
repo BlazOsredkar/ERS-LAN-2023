23b580c81f20c92445be30c8ac377bea1372361e
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2023-11-03 20:00:10 +0100

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   has_and_belongs_to_many :teams
   belongs_to :user_status
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
