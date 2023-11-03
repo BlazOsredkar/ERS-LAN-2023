4542b0dde22d57399c94751f4388ea8b6870e437
BlazOsredkar <91890538+BlazOsredkar@users.noreply.github.com>
2023-11-03 19:21:18 +0100

    added username

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,17 @@
 class User < ApplicationRecord
   #it belongs to teams with many to many
+  before_validation :downcase_username
+  validates :username, presence: true, uniqueness: true
   has_and_belongs_to_many :teams
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  private
+
+  def downcase_username
+    self.username = username.downcase if username.present?
+  end
   
 end
