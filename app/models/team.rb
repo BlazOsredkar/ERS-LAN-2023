203c47755d5f0b8c2b3a73191e1818c35bfe5b2d
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2023-11-04 17:35:33 +0100

    edited activeadmin

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,9 @@ class Team < ApplicationRecord
     #has many to many users
     has_and_belongs_to_many :users
     belongs_to :user
+
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["code", "created_at", "id", "name", "updated_at", "user_id"]
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,5 +21,13 @@ class User < ApplicationRecord
     self.username = username.downcase if username.present?
   end
 
-  
+  def self.ransackable_attributes(auth_object = nil)
+    ["confirmation_sent_at", "confirmation_token", "confirmed_at", "created_at", "current_sign_in_at", "current_sign_in_ip", "email", "encrypted_password", "has_equipment", "id", "isadmin", "last_sign_in_at", "last_sign_in_ip", "name", "remember_created_at", "reset_password_sent_at", "reset_password_token", "sign_in_count", "surname", "unconfirmed_email", "updated_at", "user_status_id", "username"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["teams", "user_status"]
+  end
+
+
 end
