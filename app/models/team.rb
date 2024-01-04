4fc5e338f920e103bd74c60afa1d68a7e2e74c9c
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2024-01-04 09:47:04 +0100

    .

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,6 @@
 class Team < ApplicationRecord
     #has many to many users
-    has_and_belongs_to_many :users
+    has_and_belongs_to_many :users 
     belongs_to :user
     belongs_to :game
 
@@ -8,7 +8,7 @@ class Team < ApplicationRecord
     def self.ransackable_attributes(auth_object = nil)
       ["code", "created_at", "game_id", "id", "is_verified", "name", "updated_at", "user_id"]
     end
-    
+
     def self.ransackable_associations(auth_object = nil)
       ["game", "user", "users"]
     end
