0ead793d082c9cfb13e1ff42aa3279df583bc587
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2024-01-07 01:47:26 +0100

    .

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
 
   def stream
-    render 'home/stream'
+    #check if STREAM_ACTIVE is set to true
+    if ENV['STREAM_ACTIVE'] == 'YES'
+      render 'home/stream'
+    else
+      redirect_to root_path, alert: 'Stream trenutno ni na voljo.'
+    end
   end
 end
