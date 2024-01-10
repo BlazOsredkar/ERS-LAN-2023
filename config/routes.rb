8e080d297ae6acc531d79f2088e0f844bce0093c
Blaž Osredkar <91890538+BlazOsredkar@users.noreply.github.com>
2024-01-10 08:59:22 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,17 +6,10 @@ Rails.application.routes.draw do
   post '/join/team', to: 'teams#join'
   post '/anketa', to: 'forms#submit_form'
   post '/discord', to: 'forms#submit_form_discord'
-  # config/routes.rb
   post '/discord/send', to: 'discord_notifications#send_notification'
-  get '/discord/send', to: 'discord_notifications#new_notification' # Optional: For rendering the form
-
-  #leave team route with id
+  get '/discord/send', to: 'discord_notifications#new_notification'
   delete '/leave/team/:id', to: 'teams#leave', as: 'leave_team'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "home#index", as: :root
-
   get "/verified_teams", to: "teams#verified_teams", as: :verified_teams
   get "/stream", to: "home#stream", as: :stream
 
